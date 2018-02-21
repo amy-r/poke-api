47f20f54c564c4ff6c3254a8caec501e783c2a55
Will Mitchell <wvmitchell@gmail.com>
2018-02-21 14:33:56 -0700

    add babel dependencies and convert index to es6

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const path = require('path')
-const express = require('express')
-const cors = require('express-cors')
-const bodyParser = require('body-parser')
-const request = require('request')
+import express from 'express'
+import cors from 'express-cors'
+import bodyParser from 'body-parser'
+import request from 'request'
 
 const port = process.env.port || 3001
 
-const types = require('./types')
-const pokemon = require('./pokemon')
+import types from '../types'
+import pokemon from '../pokemon'
 
 const app = express()
 
@@ -20,40 +19,26 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-app.get('/', function(request, response) {
-  response.status(200).send('Hello pokemon!')
+app.get('/', (req, res) => {
+  res.status(200).send('Hello pokemon!')
 })
 
-app.get('/types', function(request, response) {
-  response.status(200).send(types)
+app.get('/types', (req, res) => {
+  res.status(200).send(types)
 })
 
-app.get('/types/:id', function(request, response) {
-  var found = types.find(function(type) {
-    return type.id === request.params.id
-  })
-
-  if(found) {
-    response.status(200).send(found)
-  } else {
-    response.status(404).send('Not found')
-  }
+app.get('/types/:id', (req, res) => {
+  const found = types.find(type => type.id === req.params.id)
+  found ? res.status(200).send(found) : res.status(404).send('Not found')
 })
 
-app.get('/pokemon', function(request, response) {
-  response.status(200).send(pokemon)
+app.get('/pokemon', (req, res) => {
+  res.status(200).send(pokemon)
 })
 
-app.get('/pokemon/:id', function(request, response) {
-  var found = pokemon.find(function(poke) {
-    return poke.id === request.params.id
-  })
-
-  if(found) {
-    response.status(200).send(found)
-  } else {
-    response.status(404).send('Not found')
-  }
+app.get('/pokemon/:id', (req, res) => {
+  const found = pokemon.find(poke => poke.id === req.params.id)
+  found ? res.status(200).send(found) : res.status(404).send('Not found')
 })
 
 app.listen(port)
